9a9b24fefb71955443bc08d5e87a2c171df21447
AnthonyP1947 <anthonyparker2k16@gmail.com>
2023-07-07 13:30:05 -0500

    Initial commit

---
+++ app/src/main/java/com/mastercoding/fetchrewardscodingexercise/MainActivity.java
@@ -0,0 +1,31 @@
+package com.mastercoding.fetchrewardscodingexercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initializing the button from the view
+        button = (Button)findViewById(R.id.fetch_data);
+        button.setOnClickListener(new View.OnClickListener() {
+            //when the button is clicked
+            @Override
+            public void onClick(View v) {
+                //start a new activity
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
